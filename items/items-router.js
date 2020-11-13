4e40a049312357984cc9f7353922dedcb39bddeb
Jacob Ekegren <JacobEkegren@MacBook-Pro.local>
2020-11-12 16:54:22 -0800

    finished endpoints for put and delete

--- items/items-router.js
+++ items/items-router.js
@@ -34,7 +34,7 @@ router.post("/", restrict(), validateUserId(), async (req, res, next) => {
         const { itemName, itemPrice, description, categoryId } = req.body
         if (!itemName || !itemPrice || !description || !categoryId) {
             return res.status(400).json({
-                message: "Need a title and contents"
+                message: "Need an itemName, itemPrice, description, and categoryId"
             })
         }
         const newItem = await Items.add({
@@ -53,11 +53,18 @@ router.post("/", restrict(), validateUserId(), async (req, res, next) => {
 
 router.put("/:id", restrict(), validateUserId(), async (req, res, next) => {
     try {
+        const item = await Items.findById(req.params.id)
+        console.log(item)
+        if (!item) {
+            res.status(404).json({
+                message: "This item does not exist"
+            })
+        }
         const userId = req.user.id
-        console.log(req.user)
-        console.log(req.body)
-        if (userId !== req.user.id) {
-            return res.status(404).json({
+        console.log(userId)
+        
+        if (userId !== item.userId) {
+             res.status(404).json({
                 message: "You dont own this item"
             })
         }
@@ -70,7 +77,13 @@ router.put("/:id", restrict(), validateUserId(), async (req, res, next) => {
         }
         const { id } = req.params
 
-        updatedItem = await Items.update(id, changes)
+        if (!changes.itemName || !changes.itemPrice || !changes.description || !changes.categoryId) {
+            return res.status(400).json({
+                message: "Need an itemName, itemPrice, description, and categoryId"
+            })
+        }
+
+        const updatedItem = await Items.update(id, changes)
 
         res.status(202).json(updatedItem)
     } catch(err) {
@@ -80,6 +93,13 @@ router.put("/:id", restrict(), validateUserId(), async (req, res, next) => {
 
 router.delete("/:id", async (req, res, next) => {
     try {
+        const item = await Items.findById(req.params.id)
+        console.log(item)
+        if (!item) {
+            res.status(404).json({
+                message: "This item does not exist"
+            })
+        }
         const deleteItem = await remove(req.params.id)
         res.json({
             message: "The item has been removed"
