aaa20b582d7b6e9721fd44242cbb94f3dd3e423f
Jacob Ekegren <JacobEkegren@MacBook-Pro.local>
2020-11-13 17:29:59 -0800

    last time

--- data/seeds/01-cleanup.js
+++ data/seeds/01-cleanup.js
@@ -2,7 +2,4 @@ exports.seed = async function(knex) {
   await knex("users").delete()
   await knex("itemCategory").delete()
   await knex("items").delete()
-  await knex("items").truncate()
-  await knex("itemCategory").truncate()
-  await knex("users").truncate()
 }
