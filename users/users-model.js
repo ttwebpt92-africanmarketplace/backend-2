1933730f9ad625bd2572ac596bf5b4125776449a
Jacob Ekegren <JacobEkegren@MacBook-Pro.local>
2020-11-13 17:04:23 -0800

    fixed user add model

--- users/users-model.js
+++ users/users-model.js
@@ -1,7 +1,7 @@
 const db = require("../data/config")
 
 async function add(user) {
-	const [id] = await db("users").insert(user)
+	const [id] = await db("users").insert(user, "id")
 	return findById(id)
 }
 
